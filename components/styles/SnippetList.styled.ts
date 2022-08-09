9154e854c3ff991d3ee8dd007eca39859f8b25fa
rama41222 <dinushanka.nayomal@my.sliit.lk>
2022-08-09 12:16:06 +0530

    fix: min width

--- components/styles/SnippetList.styled.ts
+++ components/styles/SnippetList.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const SnippetContainer = styled.div`
   max-width: 320px;
-  min-width: 320px;
+  min-width: 300px;
   display: block;
   flex-direction: row;
   align-items: center;
